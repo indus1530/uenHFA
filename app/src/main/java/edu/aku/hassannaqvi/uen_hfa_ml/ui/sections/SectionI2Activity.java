451aeb41fc360b18f50cbfea1355f777233d1f80
fazaltech <fazal.abbas4@gmail.com>
2020-05-11 12:19:28 +0500

    sectionI2

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionI2Activity.java
@@ -0,0 +1,278 @@
+package edu.aku.hassannaqvi.uen_hfa_ml.ui.sections;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.aku.hassannaqvi.uen_hfa_ml.R;
+import edu.aku.hassannaqvi.uen_hfa_ml.contracts.FormsContract;
+import edu.aku.hassannaqvi.uen_hfa_ml.core.DatabaseHelper;
+import edu.aku.hassannaqvi.uen_hfa_ml.core.MainApp;
+import edu.aku.hassannaqvi.uen_hfa_ml.databinding.ActivitySectionI2Binding;
+import edu.aku.hassannaqvi.uen_hfa_ml.ui.other.EndingActivity;
+
+import static edu.aku.hassannaqvi.uen_hfa_ml.utils.UtilKt.openEndActivity;
+
+
+public class SectionI2Activity extends AppCompatActivity {
+
+    ActivitySectionI2Binding bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_i2);
+        bi.setCallback(this);
+        //setTitle(R.string.sssec);
+        setupSkips();
+
+
+    }
+
+
+    private void setupSkips() {
+
+        /*bi.ss04.setOnCheckedChangeListener(((radioGroup, i) -> {
+            if (i == bi.ss04b.getId()) {
+                Clear.clearAllFields(bi.ss05cv, false);
+            } else {
+                Clear.clearAllFields(bi.ss05cv, true);
+            }
+        }));*/
+
+
+        /*bi.ss07.setOnCheckedChangeListener(((radioGroup, i) -> {
+            if (i == bi.ss07h.getId() || i == bi.ss07i.getId()) {
+                Clear.clearAllFields(bi.ss08cv, false);
+                Clear.clearAllFields(bi.ss09cv, false);
+                Clear.clearAllFields(bi.ss10cv, false);
+                Clear.clearAllFields(bi.ss11cv, false);
+                Clear.clearAllFields(bi.ss12cv, false);
+            } else {
+                Clear.clearAllFields(bi.ss08cv, true);
+                Clear.clearAllFields(bi.ss09cv, true);
+                Clear.clearAllFields(bi.ss10cv, true);
+                Clear.clearAllFields(bi.ss11cv, true);
+                Clear.clearAllFields(bi.ss12cv, true);
+            }
+        }));*/
+
+
+        /*bi.ss09.setOnCheckedChangeListener(((radioGroup, i) -> {
+            if (i == bi.ss09b.getId()) {
+                Clear.clearAllFields(bi.ss10cv, false);
+                Clear.clearAllFields(bi.ss11cv, false);
+                Clear.clearAllFields(bi.ss12cv, false);
+
+            } else {
+                Clear.clearAllFields(bi.ss10cv, true);
+                Clear.clearAllFields(bi.ss11cv, true);
+                Clear.clearAllFields(bi.ss12cv, true);
+
+            }
+        }));*/
+
+
+        /*bi.ss11.setOnCheckedChangeListener(((radioGroup, i) -> {
+            if (i == bi.ss11b.getId()) {
+                Clear.clearAllFields(bi.ss12cv, false);
+                Clear.clearAllFields(bi.ss13cv, false);
+            } else {
+                Clear.clearAllFields(bi.ss12cv, true);
+                Clear.clearAllFields(bi.ss13cv, true);
+            }
+        }));*/
+
+    }
+
+
+    public void BtnContinue() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                finish();
+                startActivity(new Intent(this, EndingActivity.class).putExtra("complete", true));
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+
+    private boolean UpdateDB() {
+
+        DatabaseHelper db = MainApp.appInfo.getDbHelper();
+        int updcount = db.updatesFormColumn(FormsContract.FormsTable.COLUMN_SE, MainApp.fc.getsE());
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, "Updating Database... ERROR!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+    }
+
+
+    private void SaveDraft() throws JSONException {
+
+        JSONObject json = new JSONObject();
+
+        json.put("i0201a", bi.i0201aa.isChecked() ? "1"
+                : bi.i0201ab.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201b", bi.i0201ba.isChecked() ? "1"
+                : bi.i0201bb.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201c", bi.i0201ca.isChecked() ? "1"
+                : bi.i0201cb.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201d", bi.i0201da.isChecked() ? "1"
+                : bi.i0201db.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201e", bi.i0201ea.isChecked() ? "1"
+                : bi.i0201eb.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201f", bi.i0201fa.isChecked() ? "1"
+                : bi.i0201fb.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201g", bi.i0201ga.isChecked() ? "1"
+                : bi.i0201gb.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201h", bi.i0201ha.isChecked() ? "1"
+                : bi.i0201hb.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201i", bi.i0201ia.isChecked() ? "1"
+                : bi.i0201ib.isChecked() ? "2"
+                : bi.i0201ic.isChecked() ? "3"
+                : "-1");
+
+        json.put("i0201ja", bi.i0201ja.isChecked() ? "1" : "-1");
+
+        json.put("i0201jb", bi.i0201jb.isChecked() ? "2" : "-1");
+
+        json.put("i0201jc", bi.i0201jc.isChecked() ? "3" : "-1");
+
+        json.put("i0201jd", bi.i0201jd.isChecked() ? "4" : "-1");
+
+        json.put("i0201je", bi.i0201je.isChecked() ? "5" : "-1");
+
+        json.put("i0201k", bi.i0201ka.isChecked() ? "1"
+                : bi.i0201kb.isChecked() ? "2"
+                : bi.i0201kc.isChecked() ? "3"
+                : "-1");
+
+        json.put("i0201la", bi.i0201la.isChecked() ? "1" : "-1");
+
+        json.put("i0201lb", bi.i0201lb.isChecked() ? "2" : "-1");
+
+        json.put("i0201lc", bi.i0201lc.isChecked() ? "3" : "-1");
+
+        json.put("i0201m", bi.i0201ma.isChecked() ? "1"
+                : bi.i0201mb.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201n", bi.i0201na.isChecked() ? "1"
+                : bi.i0201nb.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201o", bi.i0201oa.isChecked() ? "1"
+                : bi.i0201ob.isChecked() ? "2"
+                : "-1");
+
+        json.put("i0201pa", bi.i0201pa.isChecked() ? "1" : "-1");
+
+        json.put("i0201pb", bi.i0201pb.isChecked() ? "2" : "-1");
+
+        json.put("i0201pc", bi.i0201pc.isChecked() ? "3" : "-1");
+
+        json.put("i0201pd", bi.i0201pd.isChecked() ? "4" : "-1");
+
+        json.put("i0201pe", bi.i0201pe.isChecked() ? "5" : "-1");
+
+        json.put("i0201pf", bi.i0201pf.isChecked() ? "6" : "-1");
+
+        json.put("i0201q", bi.i0201qa.isChecked() ? "1"
+                : bi.i0201qb.isChecked() ? "2"
+                : "-1");
+
+        MainApp.fc.setsE(String.valueOf(json));
+
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+
+    }
+
+
+    public void BtnEnd() {
+        openEndActivity(this);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "Back Press Not Allowed", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void showTooltip(@NotNull View view) {
+        if (view.getId() != View.NO_ID) {
+            String package_name = getApplicationContext().getPackageName();
+
+            // Question Number Textview ID must be prefixed with q_ e.g.: 'q_aa12a'
+            String infoid = view.getResources().getResourceName(view.getId()).replace(package_name + ":id/q_", "");
+
+            // Question info text must be suffixed with _info e.g.: aa12a_info
+            int stringRes = this.getResources().getIdentifier(infoid + "_info", "string", getApplicationContext().getPackageName());
+
+            // Fetch info text from strings.xml
+            //String infoText = (String) getResources().getText(stringRes);
+
+            // Check if string resource exists to avoid crash on missing info string
+            if (stringRes != 0) {
+
+                // Fetch info text from strings.xml
+                String infoText = (String) getResources().getText(stringRes);
+
+                new AlertDialog.Builder(this)
+                        .setTitle("Info: " + infoid.toUpperCase())
+                        .setMessage(infoText)
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .show();
+            } else {
+                Toast.makeText(this, "No information available on this question.", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(this, "No ID Associated with this question.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+}
