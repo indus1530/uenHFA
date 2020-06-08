69c9688d1bec380a798ac3191eeb1cca54ec7cf7
SAJID LATIF <indus1530@gmail.com>
2020-06-08 13:57:09 +0500

    SectionH0602 skip

--- app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionH6Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionH6Activity.java
@@ -5,10 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
@@ -16,6 +12,9 @@ import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import edu.aku.hassannaqvi.uen_hfa_ml.R;
 import edu.aku.hassannaqvi.uen_hfa_ml.contracts.FormsContract;
 import edu.aku.hassannaqvi.uen_hfa_ml.core.DatabaseHelper;
@@ -44,9 +43,9 @@ public class SectionH6Activity extends AppCompatActivity {
 
     private void setupSkips() {
 
-        bi.h0601.setOnCheckedChangeListener(((radioGroup, i) -> {
-            if (i == bi.h0601b.getId()) {
-                Clear.clearAllFields(bi.fldGrpCVh0601aa);
+        bi.h0602.setOnCheckedChangeListener(((radioGroup, i) -> {
+            if (i == bi.h0602b.getId()) {
+                Clear.clearAllFields(bi.llh0603);
             }
         }));
 
