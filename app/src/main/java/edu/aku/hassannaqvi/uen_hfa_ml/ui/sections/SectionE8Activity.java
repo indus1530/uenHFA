5ccb3c4355b32dd86821d27c9fed1b5036de4bc4
SAJID LATIF <indus1530@gmail.com>
2020-06-10 15:50:55 +0500

    section E8 binding variable changes in layout & savedraft

--- app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionE8Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionE8Activity.java
@@ -4,15 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import com.validatorcrawler.aliazaz.Validator;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import edu.aku.hassannaqvi.uen_hfa_ml.R;
+import edu.aku.hassannaqvi.uen_hfa_ml.core.MainApp;
 import edu.aku.hassannaqvi.uen_hfa_ml.databinding.ActivitySectionE8Binding;
 import edu.aku.hassannaqvi.uen_hfa_ml.ui.other.SectionMainActivity;
 
@@ -44,65 +41,63 @@ public class SectionE8Activity extends AppCompatActivity {
     }
 
 
-    private void SaveDraft() throws JSONException {
+    private void SaveDraft() {
 
-        JSONObject json = new JSONObject();
-        json.put("e0801", bi.e0801a.isChecked() ? "1"
+        MainApp.fc.e0801 = bi.e0801a.isChecked() ? "1"
                 : bi.e0801b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0802", bi.e0802a.isChecked() ? "1"
+        MainApp.fc.e0802 = bi.e0802a.isChecked() ? "1"
                 : bi.e0802b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0803", bi.e0803a.isChecked() ? "1"
+        MainApp.fc.e0803 = bi.e0803a.isChecked() ? "1"
                 : bi.e0803b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0804", bi.e0804a.isChecked() ? "1"
+        MainApp.fc.e0804 = bi.e0804a.isChecked() ? "1"
                 : bi.e0804b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0805", bi.e0805a.isChecked() ? "1"
+        MainApp.fc.e0805 = bi.e0805a.isChecked() ? "1"
                 : bi.e0805b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0806", bi.e0806a.isChecked() ? "1"
+        MainApp.fc.e0806 = bi.e0806a.isChecked() ? "1"
                 : bi.e0806b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0807", bi.e0807a.isChecked() ? "1"
+        MainApp.fc.e0807 = bi.e0807a.isChecked() ? "1"
                 : bi.e0807b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0808", bi.e0808a.isChecked() ? "1"
+        MainApp.fc.e0808 = bi.e0808a.isChecked() ? "1"
                 : bi.e0808b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0809", bi.e0809a.isChecked() ? "1"
+        MainApp.fc.e0809 = bi.e0809a.isChecked() ? "1"
                 : bi.e0809b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0810", bi.e0810a.isChecked() ? "1"
+        MainApp.fc.e0810 = bi.e0810a.isChecked() ? "1"
                 : bi.e0810b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0811", bi.e0811a.isChecked() ? "1"
+        MainApp.fc.e0811 = bi.e0811a.isChecked() ? "1"
                 : bi.e0811b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0812", bi.e0812a.isChecked() ? "1"
+        MainApp.fc.e0812 = bi.e0812a.isChecked() ? "1"
                 : bi.e0812b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0813", bi.e0813a.isChecked() ? "1"
+        MainApp.fc.e0813 = bi.e0813a.isChecked() ? "1"
                 : bi.e0813b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("e0814", bi.e0814a.isChecked() ? "1"
+        MainApp.fc.e0814 = bi.e0814a.isChecked() ? "1"
                 : bi.e0814b.isChecked() ? "2"
-                : "-1");
-
+                : "-1";
     }
 
 
@@ -112,13 +107,8 @@ public class SectionE8Activity extends AppCompatActivity {
 
 
     public void BtnContinue() {
-
         if (formValidation()) {
-            try {
-                SaveDraft();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            SaveDraft();
             if (UpdateDB()) {
                 finish();
                 startActivity(new Intent(this, SectionMainActivity.class));
