844f3e0d5c0627b017ab3007c428db07a7bac5b4
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-06-12 12:27:22 +0500

    fixed radiobutton variable layout and savedraft in section K9.

--- app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionK9Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionK9Activity.java
@@ -90,9 +90,9 @@ public class SectionK9Activity extends AppCompatActivity {
         MainApp.fc.k0901b = bi.k0901ba.isChecked() ? "21"
                 : bi.k0901bb.isChecked() ? "22"
                 : bi.k0901bc.isChecked() ? "23"
+                : bi.k0901c.isChecked() ? "3"
                 : "-1";
 
-//        json.put("k0901c", "-1");
 
         MainApp.fc.k0902a = bi.k0902aa.isChecked() ? "11"
                 : bi.k0902ab.isChecked() ? "12"
@@ -102,9 +102,9 @@ public class SectionK9Activity extends AppCompatActivity {
         MainApp.fc.k0902b = bi.k0902ba.isChecked() ? "21"
                 : bi.k0902bb.isChecked() ? "22"
                 : bi.k0902bc.isChecked() ? "23"
+                : bi.k0902c.isChecked() ? "3"
                 : "-1";
 
-//        json.put("k0902c", "-1");
 
         MainApp.fc.k0903a = bi.k0903aa.isChecked() ? "11"
                 : bi.k0903ab.isChecked() ? "12"
@@ -114,9 +114,9 @@ public class SectionK9Activity extends AppCompatActivity {
         MainApp.fc.k0903b = bi.k0903ba.isChecked() ? "21"
                 : bi.k0903bb.isChecked() ? "22"
                 : bi.k0903bc.isChecked() ? "23"
+                : bi.k0903c.isChecked() ? "3"
                 : "-1";
 
-//        json.put("k0903c", "-1");
 
         MainApp.fc.k0904a = bi.k0904aa.isChecked() ? "11"
                 : bi.k0904ab.isChecked() ? "12"
@@ -126,9 +126,9 @@ public class SectionK9Activity extends AppCompatActivity {
         MainApp.fc.k0904b = bi.k0904ba.isChecked() ? "21"
                 : bi.k0904bb.isChecked() ? "22"
                 : bi.k0904bc.isChecked() ? "23"
+                : bi.k0904c.isChecked() ? "3"
                 : "-1";
 
-//        json.put("k0904c", "-1");
 
         MainApp.fc.k0905a = bi.k0905aa.isChecked() ? "11"
                 : bi.k0905ab.isChecked() ? "12"
@@ -138,9 +138,8 @@ public class SectionK9Activity extends AppCompatActivity {
         MainApp.fc.k0905b = bi.k0905ba.isChecked() ? "21"
                 : bi.k0905bb.isChecked() ? "22"
                 : bi.k0905bc.isChecked() ? "23"
+                : bi.k0905c.isChecked() ? "3"
                 : "-1";
-
-//        json.put("k0905c", "-1");
 
     }
 
