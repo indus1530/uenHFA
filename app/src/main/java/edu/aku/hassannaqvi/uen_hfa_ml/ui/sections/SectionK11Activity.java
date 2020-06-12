691a134b2da3b281cc551f255908ab2463fad6b7
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-06-12 12:08:09 +0500

    Fixed layout binding changes and savedraft in Section K11.

--- app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionK11Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionK11Activity.java
@@ -5,16 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.validatorcrawler.aliazaz.Validator;
-
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
 import edu.aku.hassannaqvi.uen_hfa_ml.R;
+import edu.aku.hassannaqvi.uen_hfa_ml.core.MainApp;
 import edu.aku.hassannaqvi.uen_hfa_ml.databinding.ActivitySectionK11Binding;
 import edu.aku.hassannaqvi.uen_hfa_ml.ui.other.SectionMainActivity;
 
@@ -77,55 +78,55 @@ public class SectionK11Activity extends AppCompatActivity {
     }
 
 
-    private void SaveDraft() throws JSONException {
+    private void SaveDraft() {
 
         JSONObject json = new JSONObject();
 
-        json.put("k1101", bi.k1101a.isChecked() ? "1"
+        MainApp.fc.k1101 = bi.k1101a.isChecked() ? "1"
                 : bi.k1101b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1102", bi.k1102a.isChecked() ? "1"
+        MainApp.fc.k1102 = bi.k1102a.isChecked() ? "1"
                 : bi.k1102b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1103", bi.k1103a.isChecked() ? "1"
+        MainApp.fc.k1103 = bi.k1103a.isChecked() ? "1"
                 : bi.k1103b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1104", bi.k1104a.isChecked() ? "1"
+        MainApp.fc.k1104 = bi.k1104a.isChecked() ? "1"
                 : bi.k1104b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1105", bi.k1105a.isChecked() ? "1"
+        MainApp.fc.k1105 = bi.k1105a.isChecked() ? "1"
                 : bi.k1105b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1106", bi.k1106a.isChecked() ? "1"
+        MainApp.fc.k1106 = bi.k1106a.isChecked() ? "1"
                 : bi.k1106b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1107", bi.k1107a.isChecked() ? "1"
+        MainApp.fc.k1107 = bi.k1107a.isChecked() ? "1"
                 : bi.k1107b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1108", bi.k1108a.isChecked() ? "1"
+        MainApp.fc.k1108 = bi.k1108a.isChecked() ? "1"
                 : bi.k1108b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1109", bi.k1109a.isChecked() ? "1"
+        MainApp.fc.k1109 = bi.k1109a.isChecked() ? "1"
                 : bi.k1109b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1110", bi.k1110a.isChecked() ? "1"
+        MainApp.fc.k1110 = bi.k1110a.isChecked() ? "1"
                 : bi.k1110b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k1111", bi.k1111a.isChecked() ? "1"
+        MainApp.fc.k1111 = bi.k1111a.isChecked() ? "1"
                 : bi.k1111b.isChecked() ? "2"
-                : "-1");
+                : "-1";
 
-        json.put("k0905c", "-1");
+//        json.put("k0905c", "-1");
 
     }
 
