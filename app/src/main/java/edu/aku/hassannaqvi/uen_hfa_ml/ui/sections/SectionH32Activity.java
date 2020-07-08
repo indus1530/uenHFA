eba942346cb60f8614f978d3adc2141038a91882
SAJID LATIF <indus1530@gmail.com>
2020-07-08 16:52:31 +0500

    according to new changes H33 created

--- app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionH32Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionH32Activity.java
@@ -48,7 +48,7 @@ public class SectionH32Activity extends AppCompatActivity {
         }
         if (UpdateDB()) {
             finish();
-            startActivity(new Intent(this, SectionH17Activity.class));
+            startActivity(new Intent(this, SectionH33Activity.class));
         } else {
             Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
         }
@@ -239,100 +239,6 @@ public class SectionH32Activity extends AppCompatActivity {
         json.put("h0301z0c", bi.h0301z0cy.isChecked() ? "1"
                 : bi.h0301z0cn.isChecked() ? "2"
                 : "-1");
-
-
-        json.put("h0301aa0a", bi.h0301aa0ay.isChecked() ? "1"
-                : bi.h0301aa0an.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301aa0b", bi.h0301aa0by.isChecked() ? "1"
-                : bi.h0301aa0bn.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301aa0c", bi.h0301aa0cy.isChecked() ? "1"
-                : bi.h0301aa0cn.isChecked() ? "2"
-                : "-1");
-
-
-        json.put("h0301ab0a", bi.h0301ab0ay.isChecked() ? "1"
-                : bi.h0301ab0an.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301ab0b", bi.h0301ab0by.isChecked() ? "1"
-                : bi.h0301ab0bn.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301ab0c", bi.h0301ab0cy.isChecked() ? "1"
-                : bi.h0301ab0cn.isChecked() ? "2"
-                : "-1");
-
-
-        json.put("h0301ac0a", bi.h0301ac0ay.isChecked() ? "1"
-                : bi.h0301ac0an.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301ac0b", bi.h0301ac0by.isChecked() ? "1"
-                : bi.h0301ac0bn.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301ac0c", bi.h0301ac0cy.isChecked() ? "1"
-                : bi.h0301ac0cn.isChecked() ? "2"
-                : "-1");
-
-
-        json.put("h0301ad0a", bi.h0301ad0ay.isChecked() ? "1"
-                : bi.h0301ad0an.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301ad0b", bi.h0301ad0by.isChecked() ? "1"
-                : bi.h0301ad0bn.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301ad0c", bi.h0301ad0cy.isChecked() ? "1"
-                : bi.h0301ad0cn.isChecked() ? "2"
-                : "-1");
-
-
-        json.put("h0301ae0a", bi.h0301ae0ay.isChecked() ? "1"
-                : bi.h0301ae0an.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301ae0b", bi.h0301ae0by.isChecked() ? "1"
-                : bi.h0301ae0bn.isChecked() ? "2"
-                : "-1");
-
-        json.put("h0301ae0c", bi.h0301ae0cy.isChecked() ? "1"
-                : bi.h0301ae0cn.isChecked() ? "2"
-                : "-1");
-
-
-        json.put("h03021", bi.h03021a.isChecked() ? "1"
-                : bi.h03021b.isChecked() ? "2"
-                : bi.h03021c.isChecked() ? "3"
-                : bi.h03021d.isChecked() ? "4"
-                : "-1");
-
-
-        json.put("h03022", bi.h03022a.isChecked() ? "1"
-                : bi.h03022b.isChecked() ? "2"
-                : bi.h03022c.isChecked() ? "3"
-                : bi.h03022d.isChecked() ? "4"
-                : "-1");
-
-
-        json.put("h03023", bi.h03023a.isChecked() ? "1"
-                : bi.h03023b.isChecked() ? "2"
-                : bi.h03023c.isChecked() ? "3"
-                : bi.h03023d.isChecked() ? "4"
-                : "-1");
-
-
-        json.put("h03024", bi.h03024a.isChecked() ? "1"
-                : bi.h03024b.isChecked() ? "2"
-                : bi.h03024c.isChecked() ? "3"
-                : bi.h03024d.isChecked() ? "4"
-                : "-1");
-
 
         try {
             JSONObject json_merge = JSONUtils.mergeJSONObjects(new JSONObject(fc.getsH()), json);
