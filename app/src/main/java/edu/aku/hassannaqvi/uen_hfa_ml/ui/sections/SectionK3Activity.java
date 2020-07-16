50219229c949ad9b61e56917a1d8d1a2a633d3b7
SAJID LATIF <indus1530@gmail.com>
2020-07-16 18:07:17 +0500

    SectionK3Activity modified to latest

--- app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionK3Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_hfa_ml/ui/sections/SectionK3Activity.java
@@ -2,11 +2,15 @@ package edu.aku.hassannaqvi.uen_hfa_ml.ui.sections;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
 import org.json.JSONException;
@@ -30,10 +34,37 @@ public class SectionK3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section_k3);
         bi.setCallback(this);
+        setupSkips();
 
     }
 
 
+    private void setupSkips() {
+
+        bi.k0031.setOnCheckedChangeListener(((radioGroup, i) -> {
+            Clear.clearAllFields(bi.llk0031);
+        }));
+
+
+        bi.k0035.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (TextUtils.isEmpty(bi.k0035.getText()))
+                    return;
+                bi.k0036.setMaxvalue(Integer.parseInt(bi.k0035.getText().toString()));
+                bi.k0037d.setMaxvalue(Integer.parseInt(bi.k0035.getText().toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+    }
 
 
     public void BtnContinue() {
@@ -68,26 +99,38 @@ public class SectionK3Activity extends AppCompatActivity {
 
         JSONObject json = new JSONObject();
 
-        json.put("k0301", bi.k0301a.isChecked() ? "1"
-                : bi.k0301b.isChecked() ? "2"
+        json.put("k0031", bi.k0031a.isChecked() ? "1"
+                : bi.k0031b.isChecked() ? "2"
+                : bi.k0031c.isChecked() ? "3"
                 : "-1");
 
-        json.put("k0302", bi.k0302a.isChecked() ? "1"
-                : bi.k0302b.isChecked() ? "2"
-                : bi.k0302c.isChecked() ? "3"
+        json.put("k0032", bi.k0032a.isChecked() ? "1"
+                : bi.k0032b.isChecked() ? "2"
+                : bi.k0032c.isChecked() ? "3"
                 : "-1");
 
-        json.put("k0303", bi.k0303a.isChecked() ? "1"
-                : bi.k0303b.isChecked() ? "2"
+        json.put("k0033", bi.k0033a.isChecked() ? "1"
+                : bi.k0033b.isChecked() ? "2"
                 : "-1");
 
-        json.put("k0304", bi.k0304a.isChecked() ? "1"
-                : bi.k0304b.isChecked() ? "2"
+        json.put("k0034", bi.k0034a.isChecked() ? "1"
+                : bi.k0034b.isChecked() ? "2"
                 : "-1");
 
-        json.put("k0305", bi.k0305a.isChecked() ? "1"
-                : bi.k0305b.isChecked() ? "2"
-                : bi.k0305c.isChecked() ? "3"
+        json.put("k0035", bi.k0035.getText().toString().trim().isEmpty() ? "-1" : bi.k0035.getText().toString());
+
+        json.put("k0036", bi.k0036.getText().toString().trim().isEmpty() ? "-1" : bi.k0036.getText().toString());
+
+        json.put("k0037", bi.k0037a.isChecked() ? "1"
+                : bi.k0037b.isChecked() ? "2"
+                : bi.k0037c.isChecked() ? "3"
+                : "-1");
+        json.put("k0037d", bi.k0037d.getText().toString().trim().isEmpty() ? "-1" : bi.k0037d.getText().toString());
+
+        json.put("k0038", bi.k0038a.isChecked() ? "1"
+                : bi.k0038b.isChecked() ? "2"
+                : bi.k0038c.isChecked() ? "3"
+                : bi.k0038d.isChecked() ? "4"
                 : "-1");
 
         try {
@@ -103,7 +146,7 @@ public class SectionK3Activity extends AppCompatActivity {
 
 
     private boolean formValidation() {
-        return Validator.emptyCheckingContainer(this, bi.GrpNameSectionK3);
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
     }
 
 
